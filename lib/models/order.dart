7f018cb2eb1d3f84b2f761f1e78577c1c1625d12
Swaraj961 <swarajroutray961@gmail.com>
2020-05-01 00:52:04 +0530

    initial

---
+++ lib/models/order.dart
@@ -0,0 +1,16 @@
+import 'food.dart';
+import 'restaurant.dart';
+
+class Order {
+  final Restaurant restaurant;
+  final Food food;
+  final String date;
+  final int quantity;
+
+  Order({
+    this.date,
+    this.restaurant,
+    this.food,
+    this.quantity,
+  });
+}
